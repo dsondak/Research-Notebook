03b82da6b5cd0621e5ea4bd0cb431ab18829c038
David Sondak <dsondak@ices.utexas.edu>
2016-08-30 16:45:47 -0500

    Generation of August figures.

---
+++ figures/2016/August/config.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# SET UP PLOT PARAMETERS FOR ALL PLOTS
+pars = {  'backend'             : 'pdf',
+          'text.usetex'         : True,
+          'text.fontsize'       : 32,
+          'xtick.labelsize'     : 30,
+          'xtick.major.pad'     : 10,
+          'xtick.major.size'    : 10,
+          'xtick.minor.size'    : 5,
+          'xtick.major.width'   : 2.5,
+          'xtick.minor.width'   : 1.25,
+          'ytick.labelsize'     : 30,
+          'ytick.major.pad'     : 8,
+          'ytick.major.size'    : 10,
+          'ytick.minor.size'    : 5,
+          'ytick.major.width'   : 2.5,
+          'ytick.minor.width'   : 1.25,
+          'legend.borderpad'    : 0.25,     # empty space around the legend box
+          'legend.labelspacing' : 0.5,
+          'legend.borderaxespad': 0.75,
+          'legend.fontsize'     : 32,
+          'legend.fancybox'     : True,
+          'legend.shadow'       : False,
+          'legend.frameon'      : True,
+          'lines.markersize'    : 7,
+          'lines.linewidth'     : 4,
+          'font.family'         : 'serif',
+          'font.sans-serif'     : 'Helvetica',
+          'font.serif'          : 'cmr10',
+          'font.size'           : 32,
+          'axes.facecolor'      : 'white',   # axes background color
+          'axes.edgecolor'      : 'black',   # axes edge color
+          'axes.linewidth'      : 1.0,     # edge linewidth
+          'axes.labelweight'    : 'normal',  # weight of the x and y labels
+          'axes.labelcolor'     : 'black'
+          }
+################################################################################
+# COLORS
+red            = 'red'
+green          = 'green'
+blue           = 'blue'
+cyan           = 'cyan'
+yellow         = 'yellow'
+magenta        = 'magenta'
+black          = 'black'
+orange         = '#CC3300'
+purple         = '#700880'
+sienna         = '#A0522D'
+ForestGreen    = '#228B22'
+GoldenRod      = '#DAA520'
+CornflowerBlue = '#6495ED'
+Crimson        = '#DC143C'
+DeepPink       = '#FF1493'
+DodgerBlue     = '#1E90FF'
+SteelBlue      = '#4682B4'
+SkyBlue        = '#87CEEB'
+AquaMarine     = '#7FFFD4'
+NeonGreen      = '#7FFF00'
+DarkMagenta    = '#8B008B'
+DarkOrange     = '#FF8C00'
+Chocolate      = '#D2691E'
+SlateGray      = '#708090'
+DarkSlateGray  = '#2F4F4F'
+SaddleBrown    = '#8B4513'
+
+# LINESTYLES
+solid   = 'solid'
+dashed  = 'dashed'
+dashdot = 'dash_dot'
+dotted  = 'dotted'
+
+# MARKER SHAPES
+none = ' '
+circle = 'o'
+diamond = 'D'
+square = 's'
+hexagon = 'H'
+triright = '>'
+trileft = '<'
+ex = 'x'
+triup = '^'
+pentagon = 'p'
+star = '*'
+clubs = r'$\clubsuit$'
+bowtie = r'$\bowtie$'
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ figures/2016/August/global_T.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import config
+
+plt.rcParams.update(config.pars)
+
+Tmin = 450.0
+Tmax = 3000.0
+delta = 1.0
+
+T = np.linspace(Tmin, Tmax, 10000)
+
+Tag  = 0.0
+Tigg = 919.0
+Tadg = 2680.0
+global_react_1 = 0.5 * np.exp(-Tag / T) * (np.tanh((T - Tigg) / delta) - np.tanh((T - Tadg) / delta))
+
+Tag  = 250.0
+Tigg = 919.0
+Tadg = 2680.0
+global_react_2 = 0.5 * np.exp(-Tag / T) * (np.tanh((T - Tigg) / delta) - np.tanh((T - Tadg) / delta))
+
+Tag  = 500.0
+Tigg = 919.0
+Tadg = 2680.0
+global_react_3 = 0.5 * np.exp(-Tag / T) * (np.tanh((T - Tigg) / delta) - np.tanh((T - Tadg) / delta))
+
+Tag  = 750.0
+Tigg = 919.0
+Tadg = 2680.0
+global_react_4 = 0.5 * np.exp(-Tag / T) * (np.tanh((T - Tigg) / delta) - np.tanh((T - Tadg) / delta))
+
+Tag  = 1000.0
+Tigg = 919.0
+Tadg = 2680.0
+global_react_5 = 0.5 * np.exp(-Tag / T) * (np.tanh((T - Tigg) / delta) - np.tanh((T - Tadg) / delta))
+
+fig, ax = plt.subplots(1,1, figsize=(15,10))
+
+ax.plot(T, global_react_1, lw=5, ls='-',  label=r'$T_{ag} = 0$')
+ax.plot(T, global_react_2, lw=5, ls='--', label=r'$T_{ag} = 250$')
+ax.plot(T, global_react_3, lw=5, ls='-.', label=r'$T_{ag} = 250$')
+ax.plot(T, global_react_4, lw=5, ls='-.', label=r'$T_{ag} = 750$')
+ax.plot(T, global_react_5, lw=5, ls=':', label=r'$T_{ag} = 1000$')
+
+ax.legend(loc='best')
+
+ax.set_xlabel(r'$T$')
+ax.set_ylabel(r'$g\left(T\right)$')
+
+fig.savefig('global_T.pdf')
+
+plt.show()
